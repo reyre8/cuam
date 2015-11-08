3250b9643932f98e763b9a230b3e8d0d76b4b2e4
root <root@192-168-1-6.tpgi.com.au>
2015-11-08 22:04:22 +1100

    Loaded jquery bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
-<?php 
+<script src="dependencies/jquery-1.11.3.min.js"></script>
+<script src="dependencies/bootstrap-3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="dependencies/bootstrap-3.3.5/css/bootstrap.min.css" />
 
-echo 'Hi';
+<style>
+    
+    .top-header-bar {
+        display: block;
+        padding: 15px 20px;
+        font-weight: bold;
+        color: #fff;
+        text-align: center;
+        background-color: #0275d8;
+        font-family: "Helvetica Neue,Helvetica,Arial,sans-serif";
+        font-size: 14px;
+    }   
+    
+</style>
+
+<div class="top-header-bar">Servicios al Estudiante</div>
+<div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <nav id="bs-navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="../getting-started/">Estudiante</a>
+        </li>
+        <li>
+          <a href="../css/">Menciones</a>
+        </li>
+        <li class="active">
+          <a href="../components/">Inscripciones</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="">Salir</a></li>
+      </ul>
+    </nav>
+  </div>
